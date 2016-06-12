e939e6d7b6c02afaa0ba4b4520ec933773dd3379
Sean Mooney <bable4@gmail.com>
2016-06-12 17:33:20 -0500

    Add a basic socker-io server.
    * run `node index.js` to start a server that serves the chat application
      and uses websockets to enable communication between the clients.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChatContainer from './components/chat-container.jsx';
+import io from 'socket.io-client';
+
+const socket = io();
+
+
+const chatProperties = {
+    items: [chatItem('sean', 'message1'), chatItem('marta', 'message2')],
+    chatInput: {
+        name: 'Sean',
+        onMessageChange: messageOnChange,
+        message: 'Half of someth',
+        sendMessage: sendMessage,
+        sendText: 'Send'
+    }
+}
 
 function messageOnChange(evt) {
     chatProperties.chatInput.message = evt.target.value;
     render();
 }
 
+function addMessage(msg) {
+    chatProperties.items.push(chatItem(msg.name, msg.message));
+    render();
+}
+
 function sendMessage(evt) {
-    chatProperties.items.push(
-            chatItem(
-                chatProperties.chatInput.name,
-                chatProperties.chatInput.message));
+    socket.emit('chat message', {
+        name: chatProperties.chatInput.name,
+        message: chatProperties.chatInput.message
+    });
+
     chatProperties.chatInput.message = '';
 
     render();
@@ -28,17 +49,10 @@ function render() {
     ReactDOM.render(<ChatContainer {...chatProperties}/>, document.getElementById('chat'));
 }
 
-
-const chatProperties = {
-    items: [chatItem('sean', 'message1'), chatItem('marta', 'message2')],
-    chatInput: {
-        name: 'Sean',
-        onMessageChange: messageOnChange,
-        message: 'Half of someth',
-        sendMessage: sendMessage,
-        sendText: 'Send'
-    }
-}
+socket.on('chat message', function(msg) {
+    addMessage(msg);
+    render();
+});
 
 render();
 
